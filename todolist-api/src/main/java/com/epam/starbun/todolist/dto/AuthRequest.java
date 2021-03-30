8b66020ac088e18d885250b5f699b6d1cfeff2b8
AminevRamil <Optimus1379@yandex.ru>
2021-03-30 16:42:26 +0400

    Add "user" alias for login

--- todolist-api/src/main/java/com/epam/starbun/todolist/dto/AuthRequest.java
+++ todolist-api/src/main/java/com/epam/starbun/todolist/dto/AuthRequest.java
@@ -1,11 +1,13 @@
 package com.epam.starbun.todolist.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 public class AuthRequest {
+  @JsonAlias("user")
   @NotEmpty(message = "Логин должен быть заполнен")
   private String login;
   @NotEmpty(message = "Пароль должен быть заполнен")
