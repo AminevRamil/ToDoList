ba8e1d9fc575710d1484563d170395581aa0ff9c
Ramil Aminev <Ramil_Aminev@epam.com>
2021-05-11 16:34:47 +0400

    Default locale in application.yml

--- todolist-impl/src/main/java/com/epam/starbun/todolist/config/LocaleConfig.java
+++ todolist-impl/src/main/java/com/epam/starbun/todolist/config/LocaleConfig.java
@@ -1,6 +1,7 @@
 package com.epam.starbun.todolist.config;
 
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -12,12 +13,16 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
 
-  private static final Locale DEFAULT_LOCALE = new Locale("en", "US");
+  @Value("${app.lang}")
+  private String defaultLanguage;
+
+  @Value("${app.county}")
+  private String defaultCountry;
 
   @Bean
   public LocaleResolver localeResolver() {
     CookieLocaleResolver localeResolver = new CookieLocaleResolver();
-    localeResolver.setDefaultLocale(DEFAULT_LOCALE);
+    localeResolver.setDefaultLocale(new Locale(defaultLanguage, defaultCountry));
     localeResolver.setCookieName("lang");
     localeResolver.setCookieMaxAge(3600);
     localeResolver.setCookiePath("/");
